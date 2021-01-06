bbc81636494ae9ab49962f12f47b9a043830b42f
ndsweeney <ndsweeney@gmail.com>
2021-01-06 15:04:03 +0000

    Add files via upload

---
+++ Get resources.ps1
@@ -0,0 +1,23 @@
+﻿#Connects to Azure
+
+Connect-AzAccount
+
+#Lists your Subscriptions associated with your account
+
+#ACTION - Copy the subscription you would like to access as you will need it
+
+get-azsubscription
+
+#Select the relevent subscription you want to create the resources inside of
+
+#ACTION - Paste the subscripn below where it says XXX
+
+Select-AzSubscription -SubscriptionId XXX
+
+#Exports resources in set format ready for import
+
+Get-AzResource  | Select-Object -Property Name,ResourceGroupName,Location,Type | Export-CSV C:\temp\azure-resources2.csv
+
+#disconnects Account
+
+Disconnect-AzAccount
